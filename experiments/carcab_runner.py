a7f7a0fa3483a50d674a1bef409ebe9bb8f318c9
RaulAstudillo06 <astudillo0606@gmail.com>
2022-05-16 20:01:10 -0400

    Car cab design problem runner

---
+++ experiments/carcab_runner.py
@@ -0,0 +1,81 @@
+import os
+import sys
+import torch
+
+from botorch.settings import debug
+from botorch.test_functions.synthetic import Ackley
+
+from torch import Tensor
+
+torch.set_default_dtype(torch.float64)
+torch.autograd.set_detect_anomaly(True)
+debug._set_state(False)
+
+script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+print(script_dir[:-12])
+sys.path.append(script_dir[:-12])
+
+from experiments.problems import car_cab_obj_func, CarCabDesign
+from src.experiment_manager import experiment_manager
+from src.get_noise_level import get_noise_level
+
+
+# Objective function
+input_dim = CarCabDesign().dim
+bounds = torch.tensor(CarCabDesign()._bounds)
+
+
+def obj_func(X: Tensor) -> Tensor:
+    X_unnorm = (bounds[:, 1] - bounds[:, 0]) * X + bounds[:, 0]
+    objective_X = car_cab_obj_func(X_unnorm)
+    return objective_X
+
+
+# Algos
+# algo = "Random"
+algo = "EMOV"
+# algo = "NEI"
+# algo = "TS"
+# algo = "PKG"
+
+# estimate noise level
+comp_noise_type = "logit"
+
+if False:
+    noise_level = get_noise_level(
+        obj_func,
+        input_dim,
+        target_error=0.2,
+        top_proportion=0.01,
+        num_samples=10000000,
+        comp_noise_type=comp_noise_type,
+    )
+    print(noise_level)
+
+if comp_noise_type == "probit":
+    noise_level = 0.0665
+elif comp_noise_type == "logit":
+    noise_level = 0.0565
+
+# Run experiment
+if len(sys.argv) == 3:
+    first_trial = int(sys.argv[1])
+    last_trial = int(sys.argv[2])
+elif len(sys.argv) == 2:
+    first_trial = int(sys.argv[1])
+    last_trial = int(sys.argv[1])
+
+experiment_manager(
+    problem="carcab",
+    obj_func=obj_func,
+    input_dim=input_dim,
+    comp_noise_type=comp_noise_type,
+    comp_noise=noise_level,
+    algo=algo,
+    batch_size=2,
+    num_init_queries=2 * (input_dim + 1),
+    num_max_iter=100,
+    first_trial=first_trial,
+    last_trial=last_trial,
+    restart=True,
+)
