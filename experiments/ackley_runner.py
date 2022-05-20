4e58113f45925069f273c5df29cfb746e043e260
RaulAstudillo06 <astudillo0606@gmail.com>
2022-05-19 23:58:38 -0400

    Add all noise levels

--- experiments/ackley_runner.py
+++ experiments/ackley_runner.py
@@ -40,12 +40,13 @@ algo = "EMOV"
 
 # estimate noise level
 comp_noise_type = "probit"
+noise_level_id = 3
 
 if False:
     noise_level = get_noise_level(
         obj_func,
         input_dim,
-        target_error=0.2,
+        target_error=0.1,
         top_proportion=0.01,
         num_samples=10000000,
         comp_noise_type=comp_noise_type,
@@ -53,7 +54,8 @@ if False:
     print(noise_level)
 
 if comp_noise_type == "probit":
-    noise_level = 0.1872
+    noise_levels = [0.0760, 0.1872, 0.3817]
+    noise_level = noise_levels[noise_level_id - 1]
 elif comp_noise_type == "logit":
     noise_level = 0.1569
 
