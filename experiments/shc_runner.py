af9502671bbc2cd64dfce8e649689daa54e6de45
RaulAstudillo06 <astudillo0606@gmail.com>
2022-09-10 22:34:23 -0700

    Six Hump Camel test problem

---
+++ experiments/shc_runner.py
@@ -0,0 +1,88 @@
+import os
+import sys
+import torch
+
+from botorch.settings import debug
+from botorch.test_functions.synthetic import SixHumpCamel
+from math import pi
+from torch import Tensor
+
+torch.set_default_dtype(torch.float64)
+torch.autograd.set_detect_anomaly(True)
+debug._set_state(False)
+
+script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+print(script_dir[:-12])
+sys.path.append(script_dir[:-12])
+
+from src.experiment_manager import experiment_manager
+from src.get_noise_level import get_noise_level
+
+
+# Objective function
+input_dim = 2
+
+
+def obj_func(X: Tensor) -> Tensor:
+    X_unnorm = X.clone()
+    X_unnorm[..., 0] = 6.0 * X_unnorm[..., 0] - 3.0
+    X_unnorm[..., 0] = 4.0 * X_unnorm[..., 0] - 2.0
+    six_hump_camel = SixHumpCamel()
+    objective_X = -six_hump_camel.evaluate_true(X_unnorm)
+    return objective_X
+
+
+# Algos
+# algo = "Random"
+algo = "EMOV"
+# algo = "EI"
+# algo = "NEI"
+# algo = "TS"
+# algo = "PKG"
+
+# estimate noise level
+comp_noise_type = "probit"
+noise_level_id = 2
+
+if False:
+    noise_level = get_noise_level(
+        obj_func,
+        input_dim,
+        target_error=0.1 * float(noise_level_id),
+        top_proportion=0.01,
+        num_samples=10000000,
+        comp_noise_type=comp_noise_type,
+    )
+    print(noise_level)
+
+if comp_noise_type == "probit":
+    # noise_levels = []
+    noise_level = 0.0886
+elif comp_noise_type == "logit":
+    # noise_levels = []
+    noise_level = 0.0739
+
+# noise_level = noise_levels[noise_level_id - 1]
+
+# Run experiment
+if len(sys.argv) == 3:
+    first_trial = int(sys.argv[1])
+    last_trial = int(sys.argv[2])
+elif len(sys.argv) == 2:
+    first_trial = int(sys.argv[1])
+    last_trial = int(sys.argv[1])
+
+experiment_manager(
+    problem="six_hump_camel",
+    obj_func=obj_func,
+    input_dim=input_dim,
+    comp_noise_type=comp_noise_type,
+    comp_noise=noise_level,
+    algo=algo,
+    batch_size=2,
+    num_init_queries=2 * (input_dim + 1),
+    num_max_iter=200,
+    first_trial=first_trial,
+    last_trial=last_trial,
+    restart=False,
+)
