fb5b1563be62ec55a44d07f6acc3a25a66b1d275
RaulAstudillo06 <astudillo0606@gmail.com>
2022-10-04 15:04:27 -0700

    Make top choice GP compatible with botorch 0.6

--- src/models/top_choice_gp.py
+++ src/models/top_choice_gp.py
@@ -24,7 +24,7 @@ from gpytorch.module import Module
 from gpytorch.lazy.lazy_tensor import LazyTensor
 from gpytorch.priors.smoothed_box_prior import SmoothedBoxPrior
 from gpytorch.priors.torch_priors import GammaPrior
-from linear_operator.utils.cholesky import psd_safe_cholesky
+from gpytorch.utils.cholesky import psd_safe_cholesky
 from scipy import optimize
 from torch import float32, float64, Tensor
 from torch.nn.modules.module import _IncompatibleKeys
