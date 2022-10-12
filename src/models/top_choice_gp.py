b90cf31197c47dadd6e972880f00d33dfb9ce92b
RaulAstudillo06 <astudillo0606@gmail.com>
2022-10-11 19:17:58 -0700

    Revert recent changes made to top choice GP model

--- src/models/top_choice_gp.py
+++ src/models/top_choice_gp.py
@@ -26,7 +26,7 @@ from botorch.models.transforms.input import InputTransform
 from botorch.posteriors.gpytorch import GPyTorchPosterior
 from botorch.posteriors.posterior import Posterior
 from gpytorch import settings
-from gpytorch.constraints import Positive
+from gpytorch.constraints import GreaterThan
 from gpytorch.distributions.multivariate_normal import MultivariateNormal
 from gpytorch.kernels.rbf_kernel import RBFKernel
 from gpytorch.kernels.scale_kernel import ScaleKernel
@@ -126,7 +126,7 @@ class TopChoiceGP(Model, GP):
 
         # Set optional parameters
         # Explicitly set jitter for numerical stability in psd_safe_cholesky
-        self._jitter = kwargs.get("jitter", 1e-6)
+        self._jitter = kwargs.get("jitter", 1e-5)
         # Stopping creteria in scipy.optimize.fsolve used to find f_map in _update()
         # If None, set to 1e-6 by default in _update
         self._xtol = kwargs.get("xtol")
@@ -156,11 +156,14 @@ class TopChoiceGP(Model, GP):
                     batch_shape=self.batch_shape,
                     ard_num_dims=self.dim,
                     lengthscale_prior=ls_prior,
-                    lengthscale_constraint=Positive(
-                        transform=None, initial_value=ls_prior_mode
+                    lengthscale_constraint=GreaterThan(
+                        lower_bound=1e-4, transform=None, initial_value=ls_prior_mode
                     ),
                 ),
-                outputscale_prior=SmoothedBoxPrior(a=1, b=4),
+                outputscale_prior=SmoothedBoxPrior(a=0.2, b=5),
+                outputscale_constraint=GreaterThan(
+                    lower_bound=1e-4, transform=None, initial_value=1
+                ),
             )
 
         self.covar_module = covar_module
@@ -280,31 +283,6 @@ class TopChoiceGP(Model, GP):
         pred_mean = self._prior_mean(X)
         pred_covar = self._calc_covar(X, X)
         return pred_mean, pred_covar
-
-    def _add_jitter(self, X: Tensor) -> Tensor:
-        jitter_prev = 0
-        Eye = torch.eye(X.size(-1), device=X.device, dtype=X.dtype).expand(X.shape)
-        for i in range(3):
-            jitter_new = self._jitter * (10 ** i)
-            X = X + (jitter_new - jitter_prev) * Eye
-            jitter_prev = jitter_new
-            # This may be VERY slow given upstream pytorch issue:
-            # https://github.com/pytorch/pytorch/issues/34272
-            try:
-                _ = torch.linalg.cholesky(X)
-                warnings.warn(
-                    "X is not a p.d. matrix; "
-                    f"Added jitter of {jitter_new:.2e} to the diagonal",
-                    RuntimeWarning,
-                )
-                return X
-            except RuntimeError:
-                continue
-        warnings.warn(
-            f"Failed to render X p.d. after adding {jitter_new:.2e} jitter",
-            RuntimeWarning,
-        )
-        return X
 
     def _grad_posterior_f(
         self,
@@ -865,14 +843,26 @@ class TopChoiceGP(Model, GP):
 
             output_mean, output_covar = pred_mean, pred_covar
 
-        post = MultivariateNormal(
-            mean=output_mean,
-            # output_covar is sometimes non-PSD
-            # perform a cholesky decomposition to check and amend
-            covariance_matrix=self._scaled_psd_safe_cholesky(
-                output_covar, jitter=self._jitter
-            ),
-        )
+        try:
+            if self.datapoints is None:
+                diag_jitter = torch.eye(output_covar.size(-1))
+            else:
+                diag_jitter = torch.eye(
+                    output_covar.size(-1),
+                    dtype=self.datapoints.dtype,
+                    device=self.datapoints.device,
+                )
+            diag_jitter = diag_jitter.expand(output_covar.shape)
+            diag_jitter = diag_jitter * self._jitter
+            # Preemptively adding jitter to diagonal to prevent the use of _add_jitter
+            # given that torch.cholesky may be very slow on non-pd matrix input
+            # See https://github.com/pytorch/pytorch/issues/34272
+            # TODO: remove this once torch.cholesky issue is resolved
+            output_covar = output_covar + diag_jitter
+            post = MultivariateNormal(output_mean, output_covar)
+        except RuntimeError:
+            output_covar = self._add_jitter(output_covar)
+            post = MultivariateNormal(output_mean, output_covar)
         return post
 
     # ============== botorch.models.model.Model interfaces ==============
