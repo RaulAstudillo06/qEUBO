78ed77cb42aade8348f786e7a3104c824e93715c
RaulAstudillo06 <astudillo0606@gmail.com>
2023-02-04 01:10:52 -0800

    Preferential softmax likelihood is not 1d in the sense of GPyTorch

--- src/models/likelihoods/preferential_softmax_likelihood.py
+++ src/models/likelihoods/preferential_softmax_likelihood.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 
-import warnings
-
 import torch
 
-from gpytorch.distributions import Distribution, MultitaskMultivariateNormal, base_distributions
-from gpytorch.likelihoods import _OneDimensionalLikelihood
+from gpytorch.distributions import base_distributions
+from gpytorch.likelihoods import _OneDimensionalLikelihood, Likelihood
 
 
-class PreferentialSoftmaxLikelihood(_OneDimensionalLikelihood):
+class PreferentialSoftmaxLikelihood(Likelihood):
     r"""
     Implements the Softmax likelihood used for GP preference learning.
 
@@ -23,9 +21,17 @@ class PreferentialSoftmaxLikelihood(_OneDimensionalLikelihood):
         self.num_points = num_points
 
     def forward(self, function_samples, *params, **kwargs):
-        function_samples = function_samples.reshape(function_samples.shape[:-1] + torch.Size((int(function_samples.shape[-1] / self.num_points), self.num_points)))
-        num_data, num_points = function_samples.shape[-2:]
-
+        # print(function_samples[1, ...])
+        function_samples = function_samples.reshape(
+            function_samples.shape[:-1]
+            + torch.Size(
+                (int(function_samples.shape[-1] / self.num_points), self.num_points)
+            )
+        )
+        # print(function_samples[1, ...])
+        num_points = function_samples.shape[-1]
+        # exp_logits = torch.exp(function_samples)
+        # probs = exp_logits / exp_logits.sum(dim=-1, keepdim=True)
         if num_points != self.num_points:
             raise RuntimeError("There should be %d points" % self.num_points)
 
